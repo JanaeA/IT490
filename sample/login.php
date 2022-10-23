7db1cc588d4c43d5dc9b726de12cd35feecc3e61
TheDiamondSkyv32 <dg546@njit.edu>
2022-10-23 17:39:13 -0400

    I changed something but what?

--- sample/login.php
+++ sample/login.php
@@ -1,10 +1,13 @@
-
 <?php
-require_once('../rabbitmqphp_example/path.inc');
-require_once('../rabbitmqphp_example/get_host_info.inc');
-require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+error_reporting(E_ALL);
+ini_set('display_errors', 'ON');
 
-$client = new rabbitMQClient("../rabbitmqphp_example/testRabbitMQ.ini","testServer");
+echo "<p>What is life</p>";
+require_once('rabbitmqphp_example/path.inc');
+require_once('rabbitmqphp_example/get_host_info.inc');
+require_once('rabbitmqphp_example/rabbitMQLib.inc');
+
+$client = new rabbitMQClient("rabbitmqphp_example/testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
   $msg = $argv[1];
@@ -14,10 +17,12 @@ else
   $msg = "test message";
 }
 
-$request = array();
+//$request = array();
+$request = $_POST;
 $request['type'] = "login";
 $request['username'] = $_POST['userName'];
 $request['password'] = $_POST['password'];
+$request['bhocolate_bip_bookies'] = "chocolate";
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
@@ -27,4 +32,5 @@ echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
 
+echo "<h1>help</h1>";
 ?>
