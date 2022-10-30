1eec95bd984b8342bf24b5f2a87ca33bd632d9ea
Teo Akil <teomanakil@gmail.com>
2022-10-30 18:08:20 -0400

    Added a DMZServer file as well as a lastfm class/functions file

---
+++ lastFM.php
@@ -0,0 +1,30 @@
+<?php
+class LastFM
+{
+	public $apiKey;
+	function __construct($api)
+	{
+		$this->apiKey = $api;
+	}
+	function getInfo($artist)
+	{
+		$curl =  curl_init("http://ws.audioscrobbler.com/2.0/?method=artist.getInfo&artist=$artist&api_key=".$this->apiKey);
+		curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($curl,CURLOPT_HEADER,0);
+		curl_setopt($curl,CURLOPT_TIMEOUT,3);
+		$data = curl_exec($curl);
+		curl_close($curl);
+		return $data;
+	}
+	function getSimilar($artist)
+        {
+                $curl =  curl_init("http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&artist=$artist&api_key=".$this->apiKey);
+                curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+                curl_setopt($curl,CURLOPT_HEADER,0);
+                curl_setopt($curl,CURLOPT_TIMEOUT,3);
+                $data = curl_exec($curl);
+                curl_close($curl);
+                return $data;
+        }
+
+}
