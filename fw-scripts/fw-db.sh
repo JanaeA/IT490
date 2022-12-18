c6b09c170b4d03bf8ab0599cab7ee9a69c479f3c
Alexander Zebrowski <68719090+ajz268@users.noreply.github.com>
2022-12-18 17:30:10 -0500

    deny http,https on backend, allow from broker

--- fw-scripts/fw-db.sh
+++ fw-scripts/fw-db.sh
@@ -6,9 +6,11 @@ ufw --force reset
 ufw default deny incoming
 ufw default allow outgoing
 
-ufw allow 9993/udp
-ufw allow OpenSSH
-ufw allow 5671,5672,15671,15672,4369,25672/tcp
+ufw deny http
+ufw deny https
+
+ufw allow from 10.147.19.109 to any port 9993
+ufw allow from 10.147.19.109 to any port 5671,5672,15671,15672,4369,25672
 
 ufw logging low
 
